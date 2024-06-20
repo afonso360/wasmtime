e88a98b26eb7a4896a41c0c02309fa55b7183e88
Afonso Bordado <afonsobordado@az8.co>
2024-06-20 18:47:48 +0100

    cranelift: Consteval `brif` and `br_table` during jump threading pass

--- cranelift/codegen/src/jump_threading.rs
+++ cranelift/codegen/src/jump_threading.rs
@@ -6,14 +6,14 @@ use smallvec::{smallvec, SmallVec};
 use crate::cursor::{Cursor, FuncCursor};
 use crate::dominator_tree::DominatorTree;
 use crate::flowgraph::{BlockPredecessor, ControlFlowGraph};
-use crate::ir::{Block, BlockCall, Function, InstBuilder, Opcode};
+use crate::ir::{Block, BlockCall, Function, InstBuilder, InstructionData, Opcode, Value};
 use crate::loop_analysis::LoopAnalysis;
 use crate::trace;
 use core::fmt;
 
 /// Represents a single action to be performed as part of the
 /// jump thread analysis.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum JumpThreadAction {
     /// Evaluates the possible actions that we are able to take on this block
     /// and pushes them into the action queue.
@@ -165,7 +165,6 @@ impl JumpThreadAction {
                 // jt.loop_analysis.clear();
             }
             JumpThreadAction::ReplaceWithJump(block, new_target) => {
-                debug_assert_eq!(jt.cfg.succ_iter(block).count(), 1);
                 let target_block = new_target.block(&jt.func.dfg.value_lists).clone();
                 let target_values = new_target.args_slice(&jt.func.dfg.value_lists).to_vec();
 
@@ -178,7 +177,7 @@ impl JumpThreadAction {
                 let mut cursor = FuncCursor::new(jt.func).at_bottom(block);
                 cursor.ins().jump(target_block, &target_values[..]);
 
-                // We haven't changed the successor of this function, but we have changed
+                // We may have changed the successors of this function, and we definitley changed
                 // the terminator instruction so we need to recompute the cfg for this block
                 jt.cfg.recompute_block(jt.func, block);
             }
@@ -244,12 +243,21 @@ impl<'a> JumpThreadingPass<'a> {
     }
 
     fn analyze_block(&mut self, block: Block) -> SmallVec<[JumpThreadAction; 1]> {
-        // We assume that all blocks are reachable, and that unreachable blocks
-        // were removed in previous passes.
+        let terminator = self.func.layout.last_inst(block).unwrap();
+        let terminator_opcode = self.func.dfg.insts[terminator].opcode();
+
+        let is_entry_block = self.func.layout.entry_block().unwrap() == block;
+        let succ_count = self.cfg.succ_iter(block).count();
+        let pred_count = self.cfg.pred_iter(block).count();
+
+        // During other transformations we may have ended up in a situation where this block
+        // is now unreachable. So we should delete it.
+        if pred_count == 0 && !is_entry_block {
+            return smallvec![JumpThreadAction::Delete(block)];
+        }
 
         // If all of our terminator block calls are the same we can replace it with a jump terminator.
         // This can unlock further opportunities for optimization so we should reevaluate this block again.
-        let terminator = self.func.layout.last_inst(block).unwrap();
         let branch_dests =
             self.func.dfg.insts[terminator].branch_destination(&self.func.dfg.jump_tables);
         let all_dests_equal = branch_dests.windows(2).all(|bc| {
@@ -272,6 +280,57 @@ impl<'a> JumpThreadingPass<'a> {
             ];
         }
 
+        // Const eval brif and br_table's
+        // Since our main const eval pass is part of egraphs, this is unlikely to have
+        // much of an effect. It may still be useful for frontends other than
+        // wasmtime or if we one day run these passes multiple times.
+        if matches!(terminator_opcode, Opcode::Brif | Opcode::BrTable) {
+            let branch_value = match self.func.dfg.insts[terminator] {
+                InstructionData::Brif { arg, .. } => arg,
+                InstructionData::BranchTable { arg, .. } => arg,
+                _ => unreachable!(),
+            };
+
+            if let Some(val) = self.i64_from_iconst(branch_value) {
+                let target_blockcall = match self.func.dfg.insts[terminator] {
+                    InstructionData::Brif { blocks, .. } if val != 0 => blocks[0],
+                    InstructionData::Brif { blocks, .. } if val == 0 => blocks[1],
+                    InstructionData::BranchTable { table, .. } => {
+                        let table = &self.func.stencil.dfg.jump_tables[table];
+                        let non_default_blocks = table.as_slice();
+
+                        non_default_blocks
+                            .get(val as usize)
+                            .copied()
+                            .unwrap_or_else(|| table.default_block())
+                    }
+                    _ => unreachable!(),
+                };
+
+                let mut actions =
+                    smallvec![JumpThreadAction::ReplaceWithJump(block, target_blockcall)];
+
+                // We need to reanalyze both this block, and the previous successors. This block
+                // may now have other threading opportunities, and the previous successors
+                // may now have become dead code that we should eliminate.
+                let mut reanalyze: Vec<_> = self
+                    .cfg
+                    .succ_iter(block)
+                    .filter(|block| {
+                        // We only want to reanalyze blocks that should be deleted, which means that
+                        // they must have only one predecessor before this transform. (which is us)
+                        self.cfg.pred_iter(*block).count() == 1
+                    })
+                    .chain(Some(block))
+                    .map(JumpThreadAction::Analyze)
+                    .collect();
+                reanalyze.dedup();
+                actions.extend(reanalyze.into_iter());
+
+                return actions;
+            }
+        }
+
         // If we only have one successor, and that block only has one predecessor
         // we most definitley want to merge into it. We also check if the terminator
         // is a jump instruction, even with a singlesuccessor, we can still have
@@ -279,11 +338,9 @@ impl<'a> JumpThreadingPass<'a> {
         //
         // We also don't need to worry about computing the cost of this merger
         // since it can only reduce the total cost of the function.
-        let succ_count = self.cfg.succ_iter(block).count();
         if succ_count == 1 {
             let succ = self.cfg.succ_iter(block).nth(0).unwrap();
             let succ_predecessors = self.cfg.pred_iter(succ).count();
-            let terminator_opcode = self.func.dfg.insts[terminator].opcode();
             if succ_predecessors == 1 && terminator_opcode == Opcode::Jump {
                 let merge_pred = self.cfg.pred_iter(succ).nth(0).unwrap();
 
@@ -297,11 +354,24 @@ impl<'a> JumpThreadingPass<'a> {
             }
         }
 
-        // TODO: Const evaluate br_table
-        // TODO: Const evaluate br_if
         // TODO: Selectify br_if
-        // TODO: Inline jump blocks
+        // TODO: Inline blocks with jump terminators
 
         smallvec![]
+    }
+
+    fn i64_from_iconst(&self, val: Value) -> Option<i64> {
+        let dfg = &self.func.dfg;
+        let inst = dfg.value_def(val).inst()?;
+        let constant = match dfg.insts[inst] {
+            InstructionData::UnaryImm {
+                opcode: Opcode::Iconst,
+                imm,
+            } => imm.bits(),
+            _ => return None,
+        };
+        let ty = dfg.value_type(dfg.first_result(inst));
+        let shift_amt = std::cmp::max(0, 64 - ty.bits());
+        Some((constant << shift_amt) >> shift_amt)
     }
 }
